f6a354028a2144a65ddb8ab73dd206e78f20d50b
unknown <netflixkenya13@gmail.com>
2022-06-11 14:41:48 +0800

    add bleszie

--- src/data/MenuData.js
+++ src/data/MenuData.js
@@ -2,4 +2,5 @@ export const menuData = [
   { title: "About", link: "about" },
   { title: "Homes", link: "homes" },
   { title: "Rentals", link: "rentals" },
+  { title: "Bleszie", link: "bleszie" },
 ];
